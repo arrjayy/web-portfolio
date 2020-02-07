a2c00bf1f40245484674b9c8570dd964939fe2a9
Rajan Aggarwal <rajanaggarwal@tunstall.com>
2020-02-07 11:29:18 +0000

    Imported Babel into project

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,25 @@ module.exports = {
     // 1
     entry: './src/index.js',
     // 2
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js']
+    },
+    // 3
     output: {
         path: __dirname + '/dist',
         publicPath: '/',
         filename: 'bundle.js'
     },
-    // 3
+    // 4
     devServer: {
         contentBase: './dist'
     }
